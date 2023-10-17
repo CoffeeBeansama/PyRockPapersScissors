45c0e2d9856baa91dd539752b66c140cc3d7636e
Francis Aigo <103745801+CoffeeBeansama@users.noreply.github.com>
2023-10-17 22:05:13 +0800

    update

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from network import Network
 from support import *
+from timer import Timer
 
 class Level:
     def __init__(self):
@@ -15,11 +16,14 @@ class Level:
             "Scissor": loadSprite("Sprites/scissor.png", self.spriteSize),
         }
 
+        self.turn = 1
         self.rock = self.screen.blit(self.sprites["Rock"].convert_alpha(), (200, 180))
         self.paper = self.screen.blit(self.sprites["Paper"].convert_alpha(), (340, 180))
         self.scissors = self.screen.blit(self.sprites["Scissor"].convert_alpha(), (480, 180))
 
-        self.ui = [self.rock,self.paper,self.scissors]
+        self.turnFinished = False
+
+        self.timer = Timer(300,self.resetTurn)
 
     def handleUiEvent(self):
         mousePos = pg.mouse.get_pos()
@@ -29,14 +33,28 @@ class Level:
         self.paper = self.screen.blit(self.sprites["Paper"].convert_alpha(), (340, 180))
         self.scissors = self.screen.blit(self.sprites["Scissor"].convert_alpha(), (480, 180))
 
-        for ui in self.ui:
-            if ui.collidepoint(mousePos):
+        self.getUiPressed(self.rock,mousePos,mousePressed)
+        self.getUiPressed(self.paper,mousePos,mousePressed)
+        self.getUiPressed(self.scissors,mousePos,mousePressed)
+
+    def resetTurn(self):
+        if self.turnFinished:
+            self.turnFinished = False
+
+    def getUiPressed(self,ui,mousePos,mousePressed):
+        if not self.timer.activated:
+            if ui.collidepoint(mousePos) and not self.turnFinished:
                 if mousePressed[0]:
-                    print(ui)
+                    self.turn += 1
+                    if self.turn >= 3:
+                        self.turn = 1
+                    self.turnFinished = True
+                    self.timer.activate()
 
 
 
     def update(self):
+        self.timer.update()
         self.player2Data = self.netWork.send(self.player1Data)
         self.handleUiEvent()
 
